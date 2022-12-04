4b7646218d45263cce94dbf1540a030dd448eb08
20120454 <dat13102k2@gmail.com>
2022-12-04 21:53:26 +0700

    update

---
+++ configs/connectStr.js
@@ -0,0 +1,10 @@
+module.exports = {
+    host: 'localhost',
+    port: 5432,
+    database: '20120454',
+    user: 'postgres',
+    password: 'postgres',
+    max: 30 // use up to 30 connections
+
+    // "types" - in case you want to set custom type parsers on the pool level
+};
